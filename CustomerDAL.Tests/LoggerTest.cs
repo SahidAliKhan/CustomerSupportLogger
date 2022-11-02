ee55965cc1c837ce01e16fa076569255014b7422
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-11-02 16:04:56 +0530

    Add project files.

---
+++ CustomerDAL.Tests/LoggerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerDAL;
+using NUnit.Framework;
+
+namespace CustomerDAL.Tests
+{
+    [TestFixture]
+    public class LoggerTest
+    {
+        ValidateUsers v = null;
+        public LoggerTest()
+        {
+            v = new ValidateUsers();
+        }
+
+        [TestCase(1,"Hello@123456",ExpectedResult = true)]
+        [TestCase(2, "Hello@123123", ExpectedResult = false)]
+        public bool ValidateUsers(int id, string password)
+        {
+            return v.ValidateUser(id, password);
+        }
+
+        [TestCase(1, ExpectedResult = true)]
+        [TestCase("a", ExpectedResult = false)]
+        public bool InsertLogID(int logid)
+        {
+            CustLogInfo c = new CustLogInfo();
+            c.LogID = logid;
+            return v.Insert(c);
+        }
+    }
+}
